39145fc8ddf6221ecdf61cad0bfac6a94012e23e
Sohelrana2815 <sohel152302@gmail.com>
2024-08-26 12:38:02 +0600

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 
@@ -33,6 +34,25 @@ async function run() {
     const coffeeCollection = client.db("coffees_DB").collection("coffees");
     const orderCollection = client.db("coffees_DB").collection("orders");
 
+    // Auth Related data
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          sameSite: false,
+        })
+
+        .send({ success: "true" });
+    });
+
+    // coffees related data
     // get all data
     app.get("/coffees", async (req, res) => {
       const result = await coffeeCollection.find().toArray();
@@ -52,14 +72,56 @@ async function run() {
       res.send(result);
     });
 
-    // coffees order data
+    // Order data find by email
+
+    app.get("/orders", async (req, res) => {
+      // console.log(req.query?.email);
+
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query?.email };
+      }
+
+      const result = await orderCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // coffees order data post
 
     app.post("/orders", async (req, res) => {
       const order = req.body;
-      console.log(order);
+      // console.log(order);
       const result = await orderCollection.insertOne(order);
       res.send(result);
     });
+
+    // order status update
+
+    app.patch("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const updatedStatus = req.body;
+      console.log(updatedStatus);
+
+      const updatedDoc = {
+        $set: {
+          status: updatedStatus.status,
+        },
+      };
+      const result = await orderCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
+    // delete a order data
+
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
---
+++ sample.js
@@ -0,0 +1,16 @@
+/**
+ * jwt steps
+ * 1. install jwt
+ * 2. require('jsonwebtoken')
+ * 3. send user/payload data from client side
+ * 4. create token in backend jwt.sign(user,'secret',{expiresIn: '1h'})
+ */
+
+
+
+/**
+ * Cookie parser
+ * 1. set cookie http only , secure : false (for development purpose)
+ * 2. cors / middleware 
+ * 3. client site axios settings : with credential 
+ */
